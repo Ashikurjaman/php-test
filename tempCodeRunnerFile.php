bb06e9cebfc755273ddd066605d1f57c64e6ef6d
Ashikurjaman <ashikbur007@gmail.com>
2023-01-16 11:32:11 +0600

    first commit

---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Bangladesh";
+$age = 20;
+$word = "age";
+
+echo "hello {$name}"."<br>";
+echo "<br>";
+
+echo $$word;
---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+
+
+//                                                                 day 1
+
+
+
+
+// $name = "Bangladesh";
+// $age = 20;
+// $word = "age";
+
+// echo "hello {$name}" . "<br>";
+// echo "<br>";
+
+// echo $$word;
+// define('PI', 3.1416);
+// echo "\n value of PI =" . PI;
+// echo "<br>";
+// var_dump($name);
+// echo "<br>";
+// var_dump($$word);
+// echo "<br>";
+// printf("his name %s and age %s\n", $name, $age);
+// echo "\n";
+// printf("his name %s and age %s\n", $name, $$word);
+
+// $str = "Hello" . PHP_EOL . "World!";
+// echo $str;
+// echo "<br>";
+// $addNumber = 50;
+// echo $addNumber++;
+
+
+//                                                                        day 2
